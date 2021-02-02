92734200b9f3c9b16360f98186e247484cbc44c2
bmoisesg <bmoisesg@gmail.com>
2021-02-02 10:25:10 -0600

    actualizacion instruccion.ts y expresion.ts

--- src/Abstract/Expression.ts
+++ src/Abstract/Expression.ts
@@ -3,14 +3,11 @@ import { Environment } from "../Symbol/Environment";
 
 export abstract class Expression {
 
-    public line: number;
-    public column: number;
-
-    constructor(line: number, column: number) {
-        this.line = line;
-        this.column = column;
+    constructor(public line: number, public column: number) {
+        this.line = line
+        this.column = column + 1
     }
 
-    public abstract execute(environment: Environment) : Retorno;
-    public abstract ast(id:string):void;
+    public abstract execute(environment: Environment): Retorno
+    public abstract ast(id: string): void
 }
--- src/Abstract/Instruction.ts
+++ src/Abstract/Instruction.ts
@@ -1,15 +1,13 @@
 import { Environment } from "../Symbol/Environment";
+
 export abstract class Instruction {
 
-    public line: number;
-    public column: number;
-
-    constructor(line: number, column: number) {
-        this.line = line;
-        this.column = column;
+    constructor(public line: number, public column: number) {
+        this.line = line
+        this.column = column + 1
     }
 
-    public abstract execute(environment : Environment) : any;
-    public abstract ast():void
+    public abstract execute(environment: Environment): any
+    public abstract ast(): void
 
 }
